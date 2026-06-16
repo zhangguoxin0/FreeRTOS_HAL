90e1e3c5d0a40a3e50b98c0176a0bc25db04c424
zhangguoxin0 <3024967223@qq.com>
2026-06-16 21:12:28 +0800

    计数型信号量

--- Application/App_FreeRTOS.c
+++ Application/App_FreeRTOS.c
@@ -25,27 +25,27 @@ void task2(void *pvParameters);
 TaskHandle_t task2_handler;
 
 /***********************************************************************************************************
- *                                              二值信号量相关
+ *                                                信号量相关
  ***********************************************************************************************************/
-QueueHandle_t semphore_handle; // 二值信号量句柄
+QueueHandle_t count_semphore_handle; // 信号量句柄
 
 /***********************************************************************************************************
  *                                              FreeRTOS初始化
  ***********************************************************************************************************/
 void App_FreeRTOS_Init(void)
 {
-    // 创建二值信号量
-    semphore_handle = xSemaphoreCreateBinary();
-    if (semphore_handle == NULL)
+    // 创建计数型信号量
+    count_semphore_handle = xSemaphoreCreateCounting(100, 0); // 参数：(最大值, 初始值)
+    if (count_semphore_handle == NULL)
     {
-        printf("二值信号量创建失败\n");
+        printf("计数型信号量创建失败\n");
         while (1)
         {
         }
     }
     else
     {
-        printf("二值信号量创建成功\n");
+        printf("计数型信号量创建成功\n");
     }
 
     // 创建任务
@@ -68,7 +68,7 @@ void start_task(void *pvParameters)
 }
 
 /**
- * @brief 当按键1按下时释放二值信号量
+ * @brief 当按键1按下时释放计数型信号量
  *
  * @param pvParameters
  */
@@ -82,16 +82,16 @@ void task1(void *pvParameters)
         key_value = KEY_SACN();
         if (key_value == KEY1_PRESS)
         {
-            if (semphore_handle != NULL)
+            if (count_semphore_handle != NULL)
             {
-                res = xSemaphoreGive(semphore_handle);
+                res = xSemaphoreGive(count_semphore_handle);
                 if (res == pdPASS)
                 {
-                    printf("二值信号量释放成功\n");
+                    printf("计数型信号量释放成功\n");
                 }
                 else
                 {
-                    printf("二值信号量释放失败\n");
+                    printf("计数型信号量释放失败\n");
                 }
             }
         }
@@ -100,27 +100,27 @@ void task1(void *pvParameters)
 }
 
 /**
- * @brief 获取二值信号量，当获取成功时打印信息
+ * @brief 每1s获取计数型信号量，当获取成功时打印信号量计数值
  *
  * @param pvParameters
  */
 void task2(void *pvParameters)
 {
-    uint8_t i;
     BaseType_t res;
 
     while (1)
     {
-        // 获取二值信号量
-        res = xSemaphoreTake(semphore_handle, 1000);
+        // 获取计数型信号量
+        res = xSemaphoreTake(count_semphore_handle, portMAX_DELAY);
         // 根据获取情况打印对应信息
         if (res == pdTRUE)
         {
-            printf("二值信号量获取成功%d次\n", ++i);
+            printf("计数型信号量获取成功,信号量计数值为%d\n",uxSemaphoreGetCount(count_semphore_handle));
         }
         else
         {
-            printf("二值信号量获取失败\n");
+            printf("计数型信号量获取失败\n");
         }
+        vTaskDelay(1000);
     }
 }
--- FreeRTOS/FreeRTOSConfig.h
+++ FreeRTOS/FreeRTOSConfig.h
@@ -17,7 +17,8 @@
 #define configUSE_TRACE_FACILITY 0
 #define configUSE_16_BIT_TICKS 0
 #define configIDLE_SHOULD_YIELD 1
-#define configUSE_QUEUE_SETS 1 // 启用队列
+#define configUSE_QUEUE_SETS 1          // 启用队列
+#define configUSE_COUNTING_SEMAPHORES 1 // 启用计数型信号量
 
 /***********************************************************************************************************
  *                                              内存申请配置
