fac355a533d29aa72e2767d1aab44e38c0610571
zhangguoxin0 <3024967223@qq.com>
2026-06-17 20:58:28 +0800

    事件标志组

--- Application/App_FreeRTOS.c
+++ Application/App_FreeRTOS.c
@@ -3,8 +3,7 @@
 #include "task.h"
 #include "usart.h"
 #include "KEY.h"
-#include "semphr.h"
-#include "queue.h"
+#include "event_groups.h"
 
 /***********************************************************************************************************
  *                                                任务配置
@@ -26,42 +25,30 @@ void task2(void *pvParameters);
 TaskHandle_t task2_handler;
 
 /***********************************************************************************************************
- *                                                队列集相关
+ *                                              事件标志组相关
  ***********************************************************************************************************/
-QueueSetHandle_t queueset_handle; // 队列集句柄
-
-/***********************************************************************************************************
- *                                                 队列相关
- ***********************************************************************************************************/
-QueueHandle_t queue_handle; // 队列句柄
-
-/***********************************************************************************************************
- *                                                信号量相关
- ***********************************************************************************************************/
-QueueHandle_t semphore_handle; // 信号量句柄
+EventGroupHandle_t eventgroup_handle;
+#define EVENTBIT_0 0x000001
+#define EVENTBIT_1 0x000010
 
 /***********************************************************************************************************
  *                                              FreeRTOS初始化
  ***********************************************************************************************************/
 void App_FreeRTOS_Init(void)
 {
-    // 创建队列集
-    queueset_handle = xQueueCreateSet(2);
-    if (queueset_handle == NULL)
+    // 创建事件标志组
+    eventgroup_handle = xEventGroupCreate();
+    if (eventgroup_handle == NULL)
     {
-        printf("队列集创建失败\n");
+        printf("事件标志组创建失败\n");
+        while (1)
+        {
+        }
     }
     else
     {
-        printf("队列集创建成功\n");
+        printf("事件标志组创建成功\n");
     }
-    // 创建队列
-    queue_handle = xQueueCreate(1, sizeof(uint8_t));
-    // 创建信号量
-    semphore_handle = xSemaphoreCreateBinary();
-    // 添加队列和信号量到队列集
-    xQueueAddToSet(queue_handle, queueset_handle);
-    xQueueAddToSet(semphore_handle, queueset_handle);
 
     // 创建任务
     xTaskCreate(start_task, "start_task", START_TASK_STACK_SIZE, NULL, STACK_TASK_PRIO, &start_task_handler);
@@ -83,7 +70,7 @@ void start_task(void *pvParameters)
 }
 
 /**
- * @brief 通过按键状态向队列集中添加队列或信号量
+ * @brief 读取按键按下值，根据不同键值将事件标志组相应事件位置1，模拟事件发生
  *
  * @param pvParameters
  */
@@ -96,45 +83,27 @@ void task1(void *pvParameters)
         key_value = KEY_SACN();
         if (key_value == KEY1_PRESS)
         {
-            // 当按键1按下，将键值添加到队列集的队列中
-            xQueueSend(queue_handle, &key_value, portMAX_DELAY);
+            xEventGroupSetBits(eventgroup_handle, EVENTBIT_0);
         }
         else if (key_value == KEY2_PRESS)
         {
-            // 当按键2按下，将释放二值信号量
-            xSemaphoreGive(semphore_handle);
+            xEventGroupSetBits(eventgroup_handle, EVENTBIT_1);
         }
         vTaskDelay(10);
     }
 }
 
 /**
- * @brief 获取队列集消息
+ * @brief 同时等待事件标志组中的多个事件位，当这些事件位都置1的话就执行相应的处理
  *
  * @param pvParameters
  */
 void task2(void *pvParameters)
 {
-    QueueSetMemberHandle_t member_handle;
-    uint8_t key_value;
-
+    EventBits_t event_bit;
     while (1)
     {
-        // 从队列集中读取消息
-        member_handle = xQueueSelectFromSet(queueset_handle, portMAX_DELAY);
-
-        // 队列中有内容
-        if (member_handle == queue_handle)
-        {
-            xQueueReceive(queue_handle, &key_value, portMAX_DELAY); // 获取队列中的内容
-            printf("获取到的队列数据：%d\n", key_value);
-        }
-        // 有信号量被释放
-        else if (member_handle == semphore_handle)
-        {
-            xSemaphoreTake(semphore_handle, portMAX_DELAY); // 获取二值信号量
-            printf("获取信号量成功\n");
-        }
-        // 否则member_handle = NULL
+        event_bit = xEventGroupWaitBits(eventgroup_handle, EVENTBIT_0 | EVENTBIT_1, pdTRUE, pdTRUE, portMAX_DELAY);
+        printf("等待到的事件标志位为：%.6x\n", event_bit);
     }
 }
